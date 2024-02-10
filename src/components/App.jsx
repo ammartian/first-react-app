6023cb07c604c5eace95104a5c4ede93228149c6
ammartian <ammarhakimics@gmail.com>
2024-02-10 15:42:18 +0800

    Dynamic data mapping and arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Notes";
+import notes from "../notes.js"
+
+//1. Import notes data
+//2. Take props in the notes component
+//3. loop notes dynamically
+//4. Use arrow function
 
 function App() {
     return (
         <div>
             <Header />
+            {notes.map((note) => 
+            <Note 
+                key={note.id}
+                title={note.title}
+                content={note.content}
+            />
+            )}
             <Footer />
-            <Note />
         </div>
     );
 }
